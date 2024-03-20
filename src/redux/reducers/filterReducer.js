67929b1741f964ddde3238f4aab9dc0527fc891e
TonmoyZohani <tonmoyzohani@gmail.com>
2024-03-20 23:20:53 +0600

    filter add

--- src/redux/reducers/filterReducer.js
+++ src/redux/reducers/filterReducer.js
@@ -11,14 +11,25 @@ export const initialState = {
 export const filterReducer = (state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_BRAND:
-      return {
-        ...state,
-        filters: {
-          ...state.filters,
-          brands: [...state.filters.brands, action.payload],
-        },
-      };
-      default:
-        return state
+      if (!state.filters.brands.includes(action.payload)) {
+        return {
+          ...state,
+          filters: {
+            ...state.filters,
+            brands: [...state.filters.brands, action.payload],
+          },
+        };
+      } else {
+        return {
+          ...state,
+          filters: {
+            ...state.filters,
+            brands: state.filters.brands.filter((brand) => brand !== action.payload),
+          },
+        };
+      }
+
+    default:
+      return state;
   }
 };
