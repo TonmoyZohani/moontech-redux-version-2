59a23d84ade6b8d59b6c986b174c661fec27e14c
TonmoyZohani <tonmoyzohani@gmail.com>
2024-03-20 23:39:19 +0600

    active class add

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -7,9 +7,11 @@ const Home = () => {
   const [products, setProducts] = useState([]);
   const dispatch = useDispatch();
 
-  const store = useSelector((state)=>state.filter);
+  const filterStore = useSelector((state)=>state.filter.filters);
 
-  console.log(store);
+  const {brands,stock} = filterStore;
+
+  console.log(brands,stock);
 
   useEffect(() => {
     fetch("http://localhost:5000/products")
@@ -23,18 +25,18 @@ const Home = () => {
     <div className="max-w-7xl gap-14 mx-auto my-10">
       <div className="mb-10 flex justify-end gap-5">
         <button
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `} onClick={()=>dispatch(toggleStock())}
+          className={`border px-3 py-2 rounded-full font-semibold ${stock?activeClass:null} `} onClick={()=>dispatch(toggleStock())}
         >
           In Stock
         </button>
         <button
-          className={`border px-3 py-2 rounded-full font-semibold`}
+          className={`border px-3 py-2 rounded-full font-semibold ${brands.includes('amd')?activeClass:null}`}
           onClick={() => dispatch(toggleBrand("amd"))}
         >
           AMD
         </button>
         <button
-          className={`border px-3 py-2 rounded-full font-semibold`}
+          className={`border px-3 py-2 rounded-full font-semibold ${brands.includes('intel')?activeClass:null}`}
           onClick={() => dispatch(toggleBrand("intel"))}
         >
           Intel
